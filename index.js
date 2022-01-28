6f5ccf087fc6ea956b9e7c70137d6eaaca732f74
Morgan J <40442819+MorganEJLA@users.noreply.github.com>
2022-01-27 23:24:07 -0500

    added first part of pacman game

---
+++ index.js
@@ -0,0 +1,114 @@
+//we are building the grid in javascript by declaring classes here and styling them in css //
+
+const width = 28
+
+const grid = document.querySelector(".grid");
+const scoreDisplay = document.querySelector("#score");
+let squares = []
+
+// LAYOUT //
+
+//28 * 28 = 784 in our array to symbolize a square in our grid div. 
+//decide what each item in the array is going to symbolize.0-4//
+ //0 - pac dots
+ //1- wall
+ //2-ghost-lair
+ //3-power-pellet
+ //4-empty
+
+// pacman grid // -------------------------------------------------/
+const layout = [
+    
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+    1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1,
+    1,3,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,3,1,
+    1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1,
+    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,0,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,0,1,
+    1,0,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,0,1,
+    1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,1,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,1,1,
+    1,1,1,1,1,1,0,1,1,4,4,4,4,4,4,4,4,4,4,1,1,0,1,1,1,1,1,1,
+    1,1,1,1,1,1,0,1,1,4,1,1,1,2,2,1,1,1,4,1,1,0,1,1,1,1,1,1,
+    1,1,1,1,1,1,0,1,1,4,1,2,2,2,2,2,2,1,4,1,1,0,1,1,1,1,1,1,
+    4,4,4,4,4,4,0,0,0,4,1,2,2,2,2,2,2,1,4,0,0,0,4,4,4,4,4,4,
+    1,1,1,1,1,1,0,1,1,4,1,2,2,2,2,2,2,1,4,1,1,0,1,1,1,1,1,1,
+    1,1,1,1,1,1,0,1,1,4,1,1,1,1,1,1,1,1,4,1,1,0,1,1,1,1,1,1,
+    1,1,1,1,1,1,0,1,1,4,1,1,1,1,1,1,1,1,4,1,1,0,1,1,1,1,1,1,
+    1,0,0,0,0,0,0,0,0,4,4,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,1,
+    1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1,
+    1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1,
+    1,3,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,3,1,
+    1,1,1,0,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,0,1,1,1,
+    1,1,1,0,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,0,1,1,1,
+    1,0,0,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,0,0,1,
+    1,0,1,1,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,1,1,0,1,
+    1,0,1,1,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,1,1,0,1,
+    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1 
+]
+
+// end of pacman grid // --------------------------------------------/
+
+
+//create board//
+
+function createBoard() {
+    for(let i = 0; i < layout.length; i++){
+        //create a square
+       const square = document.createElement("div")
+        //put square in grid
+        grid.appendChild(square)
+        //each time it loops it adds a square. 
+        //put square in squares array
+        squares.push(square)
+
+
+
+        //go into the layout and grab the first item of the array //
+        if (layout[i] === 0 ){
+            squares[i].classList.add("pac-dot")
+            //added all the classes of pac-dot to any element in the array that is = 0//
+        }else if(layout[i] === 1 ){
+            squares[i].classList.add("wall")
+        
+        }else if(layout[i] === 3){
+            squares[i].classList.add("power-pellet")
+        }
+
+            
+        
+            
+    } 
+    
+    
+}
+//use a for loop to create squares for our grid //
+
+createBoard();
+
+//starting position of pacman//
+//pacman current index//
+
+let pacmanCurrentIndex = 490;
+
+squares[pacmanCurrentIndex].classList.add("pacman")
+
+// moving pacman with the keys and eventlisteners //
+
+function control(e){
+   if (e.keyCode === 40){
+       console.log('pressed down')
+   }else if (e.keyCode === 39){
+       console.log('pressed right')
+   }else if (e.keyCode === 38){
+       console.log('pressed up')
+   }else if(e.keyCode === 37){
+        console.log('pressed left')
+   }
+
+}
+   //downbutton // 
+
+document.addEventListener("keyup", control)
